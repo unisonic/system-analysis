a3ba76e617073a3aab808728f256ba3985123bce
unisonic <chimpoda@gmail.com>
2023-12-20 05:13:48 +0300

    separate task 5

--- task5/task.py
+++ task5/task.py
@@ -6,8 +6,6 @@ def read_json(file):
     with open(file, 'r') as jsonfile:
         result = json.load(jsonfile)
     return result
-expA = read_json(sys.argv[1])
-expB = read_json(sys.argv[2])
 
 
 def get_object_count(exp):
@@ -18,8 +16,6 @@ def get_object_count(exp):
         else:
             result += len(obj)
     return result
-aN = get_object_count(expA)
-bN = get_object_count(expB)
     
 
 def calculate_positions(n, exp):
@@ -35,10 +31,9 @@ def calculate_positions(n, exp):
         k += 1
 
     return result
-posA = calculate_positions(aN, expA)
-posB = calculate_positions(bN, expB)
 
 
+posA, posB = [], []
 def contradict(x, y):
     if x > aN or x > bN or y > aN or y > bN:
         return False
@@ -53,8 +48,8 @@ def both_equal(x, y):
     return True
 
 
-snm = [[i] for i in range(max(aN, bN) + 1)]
-group = [i for i in range(max(aN, bN) + 1)]
+aN, bN = 0, 0
+snm, group = [], []
 def unite(x, y):
     gx = group[x]
     gy = group[y]
@@ -71,12 +66,6 @@ def unite(x, y):
             group[elem] = gy
             snm[gy].extend(snm[gx])
             snm[gx].clear()
-for i in range(1, max(aN, bN) + 1):
-    for j in range(1, max(aN, bN) + 1):
-        if i == j:
-            continue
-        if contradict(i, j) or both_equal(i, j): 
-            unite(i, j)
 
 
 def less(x, y):
@@ -87,27 +76,52 @@ def less(x, y):
     if x <= bN and y <= bN and posB[x] < posB[y]:
         return True
     return False
-order = [i + 1 for i in range(max(aN, bN))]
-for i in range(len(order)):
-    for j in range(len(order) - 1):
-        if less(order[j + 1], order[j]):
-            order[j], order[j + 1] = order[j + 1], order[j]
 
 
-result = []
-used = [0 for i in range(max(aN, bN) + 1)]
-for i in order:
-    if used[i] == 1:
-        continue
-    
-    gi = group[i]
-    if len(snm[gi]) > 1:
-        for elem in snm[gi]:
-            used[elem] = 1
-        result.append(snm[gi])
-    else:
-        used[i] = 1
-        result.append(i)
+def task(expA, expB):
+    global aN, bN, snm, group, posA, posB
+    aN = get_object_count(expA)
+    bN = get_object_count(expB)
 
-print(result) 
+    posA = calculate_positions(aN, expA)
+    posB = calculate_positions(bN, expB)
 
+    snm = [[i] for i in range(max(aN, bN) + 1)]
+    group = [i for i in range(max(aN, bN) + 1)]
+
+    for i in range(1, max(aN, bN) + 1):
+        for j in range(1, max(aN, bN) + 1):
+            if i == j:
+                continue
+            if contradict(i, j) or both_equal(i, j): 
+                unite(i, j)
+
+    order = [i + 1 for i in range(max(aN, bN))]
+    for i in range(len(order)):
+        for j in range(len(order) - 1):
+            if less(order[j + 1], order[j]):
+                order[j], order[j + 1] = order[j + 1], order[j]
+
+
+    result = []
+    used = [0 for i in range(max(aN, bN) + 1)]
+    for i in order:
+        if used[i] == 1:
+            continue
+        
+        gi = group[i]
+        if len(snm[gi]) > 1:
+            for elem in snm[gi]:
+                used[elem] = 1
+            result.append(snm[gi])
+        else:
+            used[i] = 1
+            result.append(i)
+
+    return result
+
+
+if __name__ == "__main__":
+    expA = read_json(sys.argv[1])
+    expB = read_json(sys.argv[2])
+    print(task(expA, expB))
